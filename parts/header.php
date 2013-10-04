61f4485c7d1fa3913b9ac412de9432c7dce94f8e
SimonPadbury <simon@simonpadbury.com>
2013-10-04 09:26:19 +0100

    Update header.php

--- parts/header.php
+++ parts/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
+	<title><?php is_front_page() ? bloginfo('description') : wp_title('•', true, ''); ?></title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
