a1e257eb21e512bca2ba568725342987d9822bb6
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-07-04 13:21:21 +0100

    Update enqueues.php

--- func/enqueues.php
+++ func/enqueues.php
@@ -5,19 +5,19 @@ Google CDN jQuery with a local fallback
 See http://www.wpcoke.com/load-jquery-from-cdn-with-local-fallback-for-wordpress/
 */
 if( !is_admin()){ 
-    $url = 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'; 
+    $url = 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'; 
     $test_url = @fopen($url,'r'); 
     if($test_url !== false) { 
         function load_external_jQuery() {
             wp_deregister_script('jquery'); 
-            wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'); 
+            wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'); 
             wp_enqueue_script('jquery'); 
         }
         add_action('wp_enqueue_scripts', 'load_external_jQuery'); 
     } else {
         function load_local_jQuery() {
             wp_deregister_script('jquery'); 
-            wp_register_script('jquery', get_bloginfo('template_url').'/js/jquery-1.10.2.min.js', __FILE__, false, '1.10.2', true); 
+            wp_register_script('jquery', get_bloginfo('template_url').'/js/jquery-1.11.1.min.js', __FILE__, false, '1.11.1', true); 
             wp_enqueue_script('jquery'); 
         }
     add_action('wp_enqueue_scripts', 'load_local_jQuery'); 
