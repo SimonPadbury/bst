1cfd2641902f663d533b9ebecead18cd3d856242
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-08-08 08:12:55 +0100

    Update feedback.php

--- func/feedback.php
+++ func/feedback.php
@@ -15,7 +15,7 @@ function bst_comment($comment, $args, $depth) {
     $add_below = 'div-comment';
   }
 ?>
-<?php echo $tag ?> <?php comment_class(empty( $args['has_children'] ) ? '' : 'parent') ?> id="comment-<?php comment_ID() ?>">
+<<?php echo $tag ?> <?php comment_class(empty( $args['has_children'] ) ? '' : 'parent') ?> id="comment-<?php comment_ID() ?>">
   <?php if ( 'div' != $args['style'] ) : ?>
     <div id="div-comment-<?php comment_ID() ?>" class="comment-body">
   <?php endif; ?>
