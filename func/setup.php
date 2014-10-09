9894aa5ebccf48565e2c45a13f5cc9a4a3745bb9
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-10-09 07:48:26 +0100

    Update setup.php

--- func/setup.php
+++ func/setup.php
@@ -15,10 +15,8 @@ if (! isset($content_width))
 
 function bst_search_form( $form ) {
     $form = '<form class="form-inline" role="search" method="get" id="searchform" action="' . home_url('/') . '" >
-    <div class="form-group">
-		    <input class="form-control" type="text" value="' . get_search_query() . '" name="s" id="s" />
-    </div>
-		<button type="submit" id="searchsubmit" value="'. esc_attr__('Search') .'" class="btn btn-default"><i class="glyphicon glyphicon-search"></i> '. esc_attr__('Search') .'</button>
+	<input class="form-control" type="text" value="' . get_search_query() . '" name="s" id="s" />
+	<button type="submit" id="searchsubmit" value="'. esc_attr__('Search') .'" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
     </form>';
     return $form;
 }
