ec08642e4ef73bffc6eb957905093b0921bfc4f6
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-10-09 08:04:16 +0100

    Update bst.js

--- js/bst.js
+++ js/bst.js
@@ -4,31 +4,39 @@ $(document).ready(function() {
 	$(".comment-reply-link").addClass("btn btn-default");
 
   	/*
-	"Hovernav" navbar dropdown on hover
+	HOVERNAV
+	Navbar dropdown on hover.
 	Delete this segment if you don't want it, and delete the corresponding CSS in bst.css
 	Uses jQuery Media Query - see http://www.sitepoint.com/javascript-media-queries/
 	*/
 	var mq = window.matchMedia('(min-width: 768px)');
-  if (mq.matches) {
-    $('ul.navbar-nav > li').addClass('hovernav');
-  } else {
-  	$('ul.navbar-nav > li').removeClass('hovernav');
-  };
-  	/*
-	The addClass/removeClass also needs to be triggered on page resize <=> 768px
-	*/
-  if (matchMedia) {
-    var mq = window.matchMedia('(min-width: 768px)');
-    mq.addListener(WidthChange);
-    WidthChange(mq);
-  }
+	if (mq.matches) {
+	  $('ul.navbar-nav > li').addClass('hovernav');
+	} else {
+	  $('ul.navbar-nav > li').removeClass('hovernav');
+	};
+  	// The addClass/removeClass also needs to be triggered on page resize <=> 768px
+	if (matchMedia) {
+	  var mq = window.matchMedia('(min-width: 768px)');
+	  mq.addListener(WidthChange);
+	  WidthChange(mq);
+	}
 	function WidthChange(mq) {
-    if (mq.matches) {
-      $('ul.navbar-nav > li').addClass('hovernav');
-    } else {
-      $('ul.navbar-nav > li').removeClass('hovernav');
-    }
-  };
+	  if (mq.matches) {
+	    $('ul.navbar-nav > li').addClass('hovernav');
+	  } else {
+	    $('ul.navbar-nav > li').removeClass('hovernav');
+	  }
+	};
+
+	/*
+	MEGANAV
+	Allows GRAND-CHILD links to be displayed in a mega-menu on screens larger than phones.
+	Delete this segment if you don't want it, and delete the corresponding CSS in bst.css
+	*/
+	$('.navbar').addClass('meganav');
+	$('.meganav .dropdown-menu .dropdown-menu').parent().addClass('has-children').parents('li').addClass('dropdown mega-menu');
+
 	/*
 	Forms
 	*/
