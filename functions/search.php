45e862456d031184b2fa8b99705616e51670fbec
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-10-10 07:23:57 +0100

    Create search.php

---
+++ functions/search.php
@@ -0,0 +1,12 @@
+<?php
+
+function bst_search_form( $form ) {
+    $form = '<form class="form-inline" role="search" method="get" id="searchform" action="' . home_url('/') . '" >
+	<input class="form-control" type="text" value="' . get_search_query() . '" placeholder="' . esc_attr__('Search', 'bst') . '..." name="s" id="s" />
+	<button type="submit" id="searchsubmit" value="'. esc_attr__('Search', 'bst') .'" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
+    </form>';
+    return $form;
+}
+add_filter( 'get_search_form', 'bst_search_form' );
+
+?>
