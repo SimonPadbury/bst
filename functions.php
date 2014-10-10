b6c318911f22c8bebb0a904527b2a7eaad3814d5
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-10-10 07:25:05 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -8,6 +8,7 @@ require_once locate_template('/functions/setup.php');
 require_once locate_template('/functions/enqueues.php');
 require_once locate_template('/functions/navbar.php');
 require_once locate_template('/functions/widgets.php');
+require_once locate_template('/functions/search.php');
 require_once locate_template('/functions/feedback.php');
 
 add_action('after_setup_theme', 'true_load_theme_textdomain');
