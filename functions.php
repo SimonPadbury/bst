0d0dfc49e84d033f742f5f81c0f827c04e482f16
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-10-10 07:19:55 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,14 +1,14 @@
 <?php
 /*
-All the functions are in the PHP pages in the func/ folder.
+All the functions are in the PHP pages in the functions/ folder.
 */
 
-require_once locate_template('/func/cleanup.php');
-require_once locate_template('/func/setup.php');
-require_once locate_template('/func/enqueues.php');
-require_once locate_template('/func/navbar.php');
-require_once locate_template('/func/widgets.php');
-require_once locate_template('/func/feedback.php');
+require_once locate_template('/functions/cleanup.php');
+require_once locate_template('/functions/setup.php');
+require_once locate_template('/functions/enqueues.php');
+require_once locate_template('/functions/navbar.php');
+require_once locate_template('/functions/widgets.php');
+require_once locate_template('/functions/feedback.php');
 
 add_action('after_setup_theme', 'true_load_theme_textdomain');
 
