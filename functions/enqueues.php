ee4348e3ec65e52f9d5326a8ddb26879fe030c61
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-11-20 14:05:43 +0000

    Update enqueues.php

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -3,14 +3,22 @@
 /*
 Google CDN jQuery with a local fallback
 See http://www.wpcoke.com/load-jquery-from-cdn-with-local-fallback-for-wordpress/
+
+NOTE: If this theme has trouble getting jQuery form Google (or from the browser cashe), do this:
+-- uncomment line 12 and instead comment-out line 13
+-- uncomment line 18 and instead comment-out line 19
+
+ALTERNATIVELY, seeing that WordPress is updating its onboard jQuery frequently these days, you may prefer to use that instead (see http://matthewruddy.com/using-jquery-with-wordpress/)
 */
-if( !is_admin()){ 
-    $url = 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'; 
+if (!is_admin()){ 
+//    $url = 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'; 
+    $url = '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'; 
     $test_url = @fopen($url,'r'); 
     if($test_url !== false) { 
         function load_external_jQuery() {
             wp_deregister_script('jquery'); 
-            wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'); 
+//            wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'); 
+            wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'); 
             wp_enqueue_script('jquery'); 
         }
         add_action('wp_enqueue_scripts', 'load_external_jQuery'); 
@@ -24,30 +32,40 @@ if( !is_admin()){
     }
 }
 
-function bst_enqueues()
-{
-		wp_register_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', false, null);
-		wp_enqueue_style('bootstrap-css');
+/*
+Using WordPress's onboard jQuery
+See http://matthewruddy.com/using-jquery-with-wordpress/
+
+If you decide to remove or comment out the "Google CDN jQuery with a local fallback" stuff above, then simply un-comment
+the "wp_enqueue_script( 'jquery' );" line below.
+*/
+
+function bst_enqueues() {
+	
+	//wp_enqueue_script( 'jquery' );
+
+	wp_register_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', false, null);
+	wp_enqueue_style('bootstrap-css');
 
   	wp_register_style('bst-css', get_template_directory_uri() . '/css/bst.css', false, null);
-		wp_enqueue_style('bst-css');
+	wp_enqueue_style('bst-css');
 
   	wp_register_script('modernizr', get_template_directory_uri() . '/js/modernizr-2.6.2.min.js', false, null, true);
-    wp_enqueue_script('modernizr');
+	wp_enqueue_script('modernizr');
 
-		wp_register_script('html5shiv.js', get_template_directory_uri() . '/js/html5shiv.js', false, null, true);
-    wp_enqueue_script('html5shiv.js');
+	wp_register_script('html5shiv.js', get_template_directory_uri() . '/js/html5shiv.js', false, null, true);
+	wp_enqueue_script('html5shiv.js');
 
   	wp_register_script('respond', get_template_directory_uri() . '/js/respond.min.js', false, null, true);
-    wp_enqueue_script('respond');
+	wp_enqueue_script('respond');
 
   	wp_register_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.js', false, null, true);
-    wp_enqueue_script('bootstrap-js');
+	wp_enqueue_script('bootstrap-js');
 
-    wp_register_script('bst-js', get_template_directory_uri() . '/js/bst.js', false, null, true);
-    wp_enqueue_script('bst-js');
+	wp_register_script('bst-js', get_template_directory_uri() . '/js/bst.js', false, null, true);
+	wp_enqueue_script('bst-js');
 
-    if (is_singular() && comments_open() && get_option('thread_comments')) {
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
       }
 }
