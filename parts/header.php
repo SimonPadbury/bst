7c78afc985c3ce800b766b5a7e2275f662cc908c
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-12-17 16:37:39 +0000

    Update header.php

--- parts/header.php
+++ parts/header.php
@@ -14,42 +14,7 @@
 <div class="alert alert-warning">
 	You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.
 </div>
-<![endif]-->    
-
-<?php
-/*
-Upper menubar (at screen top)
-=============================
-Delete this whole <nav>...</nav> block if you don't want it, and delete the line in functions/navbar.php that looks like this:
-register_nav_menu('upper-bar', __('Screen-top menu'));
-*/
-?>
-<nav class="navbar navbar-default navbar-static-top" role="navigation">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".upper-navbar">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a>
-    </div><!-- /.navbar-header -->
-    <div class="collapse navbar-collapse upper-navbar">    
-      <?php				
-          $args = array(
-            'theme_location' => 'upper-bar',
-            'depth' => 0,
-            'container'	=> false,
-            'fallback_cb' => false,
-            'menu_class' => 'nav navbar-nav',
-            'walker' => new BootstrapNavMenuWalker()
-          );
-          wp_nav_menu($args);
-      ?>
-	  </div><!-- /.navbar-collapse -->
-  </div>
-</nav>
+<![endif]-->
 
   <div class="container">
     <div class="row">
@@ -61,14 +26,6 @@ register_nav_menu('upper-bar', __('Screen-top menu'));
     </div>
   </div>
 
-<?php
-/*
-Lower menubar (main menubar, below site title)
-==============================================
-Delete this whole <nav>...</nav> block if you don't want it, and delete the line in functions/navbar.php that looks like this:
-register_nav_menu('lower-bar', __('Main menu (below site title)'));
-*/
-?>
   <div class="container main-nav">
     <nav class="navbar navbar-default navbar-static" role="navigation">
       <div class="navbar-header">
@@ -80,19 +37,19 @@ register_nav_menu('lower-bar', __('Main menu (below site title)'));
         </button>
         <?php /* <a class="navbar-brand" href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a> */ ?>
       </div><!-- /.navbar-header -->
-      <div class="collapse navbar-collapse lower-navbar">    
+      <div class="collapse navbar-collapse">    
         <?php				
             $args = array(
-              'theme_location' => 'lower-bar',
-              'depth' => 0,
+              'theme_location' 	=> 'navbar',
+              'depth' 		=> 0,
               'container'	=> false,
-              'fallback_cb' => false,
-              'menu_class' => 'nav navbar-nav',
-              'walker' => new BootstrapNavMenuWalker()
+              'fallback_cb' 	=> false,
+              'menu_class' 	=> 'nav navbar-nav',
+              'walker' 		=> new BootstrapNavMenuWalker()
             );
             wp_nav_menu($args);
         ?>
       </div><!-- /.navbar-collapse -->
-  	</nav>
+    </nav>
 
   </div>
