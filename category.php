f5e80727e4ae8a4fbbdf90a7f3de0e0bd3bb9aa9
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-12-27 11:01:28 +0000

    Update category.php

--- category.php
+++ category.php
@@ -1,49 +1,19 @@
-<?php get_template_part('parts/header'); ?>
+<?php get_template_part('includes/header'); ?>
 
 <div class="container">
   <div class="row">
     
     <div class="col-xs-12 col-sm-8">
       <div id="content" role="main">
-        <header class="archive-header">
-	  <h3><?php _e('Blog Category', 'bst'); ?>: <?php single_cat_title(''); ?></h3>
-          <hr/>
-        </header>
-        <?php if(have_posts()): while(have_posts()): the_post();?>
-        <article role="article" id="post_<?php the_ID()?>">
-          <header>
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title()?></a></h2>
-            <h4>
-              <em>
-                <span class="text-muted" class="author"><?php _e('By', 'bst'); echo " "; the_author() ?>,</span>
-                <time  class="text-muted" datetime="<?php the_time('d-m-Y')?>"><?php the_time('jS F Y') ?></time>
-              </em>
-            </h4>
-          </header>
-          <?php the_post_thumbnail(); ?>
-          <?php the_content('&hellip; ' . __('Continue reading', 'bst' ) . ' <i class="glyphicon glyphicon-arrow-right"></i>'); ?>
-          <p class="text-muted" style="margin-bottom: 20px;">
-            <i class="glyphicon glyphicon-folder-open"></i>&nbsp; <?php _e('Filed under', 'bst'); ?>: <?php the_category(', ') ?><br/>
-            <i class="glyphicon glyphicon-comment"></i>&nbsp; <?php _e('Comments', 'bst'); ?>: <?php comments_popup_link(__('None', 'bst'), '1', '%'); ?>
-          </p>
-          <hr/>
-        </article>
-        <?php endwhile; ?>
-        <ul class="pagination">
-          <li class="older"><?php next_posts_link('&laquo; ' . __('Older', 'bst')) ?></li>
-          <li class="newer"><?php previous_posts_link(__('Newer', 'bst') . ' &raquo;') ?></li>
-        </ul>
-        <?php else: ?>
-        <?php wp_redirect(get_bloginfo('siteurl').'/404', 404); exit; ?>
-        <?php endif;?>
+        <?php get_template_part('includes/loops/content', get_post_format()); ?>
       </div><!-- /#content -->
     </div>
     
     <div class="col-xs-6 col-sm-4" id="sidebar" role="navigation">
-       <?php get_template_part('parts/sidebar'); ?>
+       <?php get_template_part('includes/sidebar'); ?>
     </div>
     
   </div><!-- /.row -->
 </div><!-- /.container -->
 
-<?php get_template_part('parts/footer'); ?>
+<?php get_template_part('includes/footer'); ?>
