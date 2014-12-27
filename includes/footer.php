3f8ba91af43f04583d81aa860c9cfead18f13bbf
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-12-27 10:55:02 +0000

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<footer class="container site-footer">
+  <hr/>
+	<div class="row">
+    <?php dynamic_sidebar('footer-widget-area'); ?>
+  </div>
+  <hr/>
+  <div class="row">
+    <div class="col-lg-12 site-sub-footer">
+      <p>&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></p>
+    </div>
+  </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
