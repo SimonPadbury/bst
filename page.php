fe876231e2df122e23ab20ccc3e9a309cbbb4a11
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-12-27 11:02:34 +0000

    Update page.php

--- page.php
+++ page.php
@@ -1,30 +1,19 @@
-<?php get_template_part('parts/header'); ?>
+<?php get_template_part('includes/header'); ?>
 
 <div class="container">
   <div class="row">
 
     <div class="col-xs-12 col-sm-8">
       <div id="content" role="main">
-        <?php if(have_posts()): while(have_posts()): the_post();?>
-        <article role="article" id="post_<?php the_ID()?>" <?php post_class()?>>
-          <header>
-            <h2><?php the_title()?></h2>
-            <hr/>
-          </header>
-          <?php the_content()?>
-        </article>
-        <?php endwhile; ?> 
-        <?php else: ?>
-        <?php wp_redirect(get_bloginfo('siteurl').'/404', 404); exit; ?>
-        <?php endif;?>
+        <?php get_template_part('includes/loops/content', 'page'); ?>
       </div><!-- /#content -->
     </div>
     
     <div class="col-xs-6 col-sm-4" id="sidebar" role="navigation">
-      <?php get_template_part('parts/sidebar'); ?>
+      <?php get_template_part('includes/sidebar'); ?>
     </div>
     
   </div><!-- /.row -->
 </div><!-- /.container -->
 
-<?php get_template_part('parts/footer'); ?>
+<?php get_template_part('includes/footer'); ?>
