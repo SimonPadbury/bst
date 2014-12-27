5556ea8087871033038494f7865c56d7412f213d
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-12-27 10:57:51 +0000

    Create content-single.php

---
+++ includes/loops/content-single.php
@@ -0,0 +1,32 @@
+<?php
+/*
+The Single Posts Loop
+=====================
+*/
+?> 
+
+<?php if(have_posts()): while(have_posts()): the_post(); ?>
+    <article role="article" id="post_<?php the_ID()?>" <?php post_class()?>>
+        <header>
+            <h2><?php the_title()?></h2>
+            <h4>
+                <em>
+                    <span class="text-muted" class="author"><?php _e('By', 'bst'); echo " "; the_author() ?>,</span>
+                    <time  class="text-muted" datetime="<?php the_time('d-m-Y')?>"><?php the_time('jS F Y') ?></time>
+                </em>
+            </h4>
+            <p class="text-muted" style="margin-bottom: 30px;">
+                <i class="glyphicon glyphicon-folder-open"></i>&nbsp; <?php _e('Filed under', 'bst'); ?>: <?php the_category(', ') ?><br/>
+                <i class="glyphicon glyphicon-comment"></i>&nbsp; <?php _e('Comments', 'bst'); ?>: <?php comments_popup_link(__('None', 'bst'), '1', '%'); ?>
+            </p>
+        </header>
+        <section>
+            <?php the_post_thumbnail(); ?>
+            <?php the_content()?>
+        </section>
+    </article>
+<?php comments_template('/includes/loops/comments.php'); ?>
+<?php endwhile; ?>
+<?php else: ?>
+<?php wp_redirect(get_bloginfo('siteurl').'/404', 404); exit; ?>
+<?php endif; ?>
