77659c4ba395d803d763fe1ea78ddef3bf40645f
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-12-29 17:48:45 +0000

    Update setup.php

--- functions/setup.php
+++ functions/setup.php
@@ -61,31 +61,32 @@ add_theme_support('post-formats', array('aside', 'gallery', 'link', 'image', 'qu
 // Bootstrap pagination
 //Copied and modified from: http://scriptsell.net/bootstrap-pagination-in-wordpress-theme/
 
-function bst_pagination( $query=null ) {
-	global $wp_query;
-	$query = $query ? $query : $wp_query;
-	$length = 999999999; // This needs to be an unlikely integer
-	
-	$paginate = paginate_links(
-		array(
-			'base' => str_replace( $length, '%#%', esc_url( get_pagenum_link( $length ) ) ),
-			'type' => 'array',
-			'total' => $query->max_num_pages,
-			'format' => '?paged=%#%',
+// Bootstrap pagination
+
+if ( ! function_exists( 'bst_pagination' ) ) {
+	function bst_pagination() {
+		global $wp_query;
+		$big = 999999999; // This needs to be an unlikely integer
+		// For more options and info view the docs for paginate_links()
+		// http://codex.wordpress.org/Function_Reference/paginate_links
+		$paginate_links = paginate_links( array(
+			'base' => str_replace( $big, '%#%', get_pagenum_link($big) ),
 			'current' => max( 1, get_query_var('paged') ),
-			'prev_text' => __('<i class="glyphicon glyphicon-arrow-left"></i> Previous'),
-			'next_text' => __('Next <i class="glyphicon glyphicon-arrow-right"></i>'),
-		)
-	);
-	if ($query->max_num_pages > 1) :
+			'total' => $wp_query->max_num_pages,
+			'mid_size' => 5,
+			'prev_next' => True,
+			'prev_text' => __('<i class="glyphicon glyphicon-chevron-left"></i> Previous'),
+			'next_text' => __('Next <i class="glyphicon glyphicon-chevron-right"></i>'),
+			'type' => 'list'
+		) );
+		$paginate_links = str_replace( "<ul class='page-numbers'>", "<ul class='pagination'>", $paginate_links );
+		$paginate_links = str_replace( "<li><span class='page-numbers current'>", "<li class='active'><a href='#'>", $paginate_links );
+		$paginate_links = str_replace( "</span>", "</a>", $paginate_links );
+		$paginate_links = preg_replace( "/\s*page-numbers/", "", $paginate_links );
+		// Display the pagination if more than one page is found
+		if ( $paginate_links ) {
+			echo $paginate_links;
+		}
+	}
+}
 ?>
-        <ul class="pagination pagination-centered">
-        	<?php
-			foreach ( $paginate as $page ) {
-				if(!preg_match('/^<span class="page-numbers dots">/',$page)){
-					echo '<li>' . $page . '</li>';
-				}
-			}
-		?>
-        </ul>
-<?php endif; } ?>
