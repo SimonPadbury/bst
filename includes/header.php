ab2e23cc2dfc1c909f44a2815cd0c13261279549
SimonPadbury <simon_padbury@yahoo.co.uk>
2015-01-06 08:13:21 +0000

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
 	<title><?php wp_title('•', true, 'right'); bloginfo('name'); ?></title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<?php wp_head(); ?>
 </head>
 
@@ -38,9 +38,9 @@
               'walker' 		=> new BootstrapNavMenuWalker()
             );
             wp_nav_menu($args);
-        ?>
-        <?php get_template_part('includes/navbar-search'); ?>
-        <?php				
+            
+            get_template_part('includes/navbar-search');
+            
             $args = array(
               'theme_location' 	=> 'navbar-right',
               'depth' 		=> 0,
@@ -73,4 +73,5 @@ If you are displaying your site title in the "brand" link in the Bootstrap navba
     </div>
   </div>
 </div>
-*/?>
+*/
+?>
