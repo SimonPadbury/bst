3df2d618911d33e8571e3f59b5629f02a4e50891
SimonPadbury <simonpadbury@SimonPaburysMBP.default>
2015-05-13 07:41:02 +0100

    Update Bootstrap, jQuery and Modernizr

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -2,16 +2,16 @@
 
 function bst_enqueues() {
 
-	wp_register_script('jquery', get_bloginfo('template_url').'/js/jquery-1.11.2.min.js', __FILE__, false, '1.11.2', true);
+	wp_register_script('jquery', get_bloginfo('template_url').'/js/jquery-1.11.3.min.js', __FILE__, false, '1.11.3', true);
 	wp_enqueue_script( 'jquery' );
 
-	wp_register_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', false, '3.3.2', null);
+	wp_register_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', false, '3.3.4', null);
 	wp_enqueue_style('bootstrap-css');
 
   	wp_register_style('bst-css', get_template_directory_uri() . '/css/bst.css', false, null);
 	wp_enqueue_style('bst-css');
 
-  	wp_register_script('modernizr', get_template_directory_uri() . '/js/modernizr-2.6.2.min.js', false, null, true);
+  	wp_register_script('modernizr', get_template_directory_uri() . '/js/modernizr-2.8.3.min.js', false, null, true);
 	wp_enqueue_script('modernizr');
 
   	wp_register_script('respond', get_template_directory_uri() . '/js/respond.min.js', false, null, true);
