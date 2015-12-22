cb53bf34976710eba69a96ec593931f57de4f965
Simon Padbury <simon_padbury@yahoo.co.uk>
2015-12-22 16:58:36 +0000

    Create content-none.php

---
+++ includes/loops/content-none.php
@@ -0,0 +1,11 @@
+<?php
+/*
+The "No Content" error Loop
+========================
+*/
+?> 
+
+<div class="alert alert-warning">
+  <h2><i class="glyphicon glyphicon-warning-sign"></i> <?php _e('Error', 'bst'); ?> 404</h2>
+  <p><?php _e('The page you were looking for does not exist.', 'bst'); ?></p>
+</div>
