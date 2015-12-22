fe2ffa322014521bbc4a9ccc84ce725ebc13ff1c
Simon Padbury <simon_padbury@yahoo.co.uk>
2015-12-22 16:49:18 +0000

    Update enqueues.php

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -2,14 +2,17 @@
 
 function bst_enqueues() {
 
-	wp_register_script('jquery', get_bloginfo('template_url').'/js/jquery-1.11.3.min.js', __FILE__, false, '1.11.3', true);
-	wp_enqueue_script( 'jquery' );
-
+	/* Styles */
+	
 	wp_register_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', false, '3.3.4', null);
 	wp_enqueue_style('bootstrap-css');
 
   	wp_register_style('bst-css', get_template_directory_uri() . '/css/bst.css', false, null);
 	wp_enqueue_style('bst-css');
+
+	/* Scripts */
+	wp_register_script('bst-jquery', get_bloginfo('template_url').'/js/jquery-1.11.3.min.js', __FILE__, false, '1.11.3', true);
+	wp_enqueue_script( 'bst-jquery' );
 
   	wp_register_script('modernizr', get_template_directory_uri() . '/js/modernizr-2.8.3.min.js', false, null, true);
 	wp_enqueue_script('modernizr');
