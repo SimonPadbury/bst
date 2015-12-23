68beee0d2b5b4294f31cef646288a2b592bee5e6
Simon Padbury <simon_padbury@yahoo.co.uk>
2015-12-23 09:15:09 +0000

    Update content-single.php

--- includes/loops/content-single.php
+++ includes/loops/content-single.php
@@ -28,5 +28,5 @@ The Single Posts Loop
     </article>
 <?php comments_template('/includes/loops/comments.php'); ?>
 <?php endwhile; ?>
-<?php else: get_template_part('includes/loops/content', 'none'); endif; ?>
+<?php else: get_template_part('includes/loops/content', 'none'); ?>
 <?php endif; ?>
